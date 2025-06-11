6b2c56707fecad8d251b5fafe0a08c9c374378ab
maryori190909 <maryori.armengod@ucsm.edu.pe>
2025-06-11 05:01:41 -0500

    v8

--- actividad_01/listlinked/ListLinked.java
+++ actividad_01/listlinked/ListLinked.java
@@ -6,4 +6,17 @@ public class ListLinked<T> {
         head = null;
     }
 
+     public void insert_Sig(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node<T> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
 }
