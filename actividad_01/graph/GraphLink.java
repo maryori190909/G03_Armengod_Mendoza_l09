70e1abf6426bededa27dc3fba93074280851b949
maryori190909 <maryori.armengod@ucsm.edu.pe>
2025-06-11 05:35:37 -0500

    v23

--- actividad_01/graph/GraphLink.java
+++ actividad_01/graph/GraphLink.java
@@ -3,4 +3,10 @@ import listLinked.Node;
 
 public class GraphLink<E> {
 
+    protected ListLinked<Vertex<E>> listVertex;
+
+    public GraphLink() {
+        listVertex = new ListLinked<>();
+    }
+
 }
