5fd700623a688bc967566be291dd08eb957696dc
maryori190909 <maryori.armengod@ucsm.edu.pe>
2025-06-11 04:59:21 -0500

    v6

--- actividad_01/listlinked/Node.java
+++ actividad_01/listlinked/Node.java
@@ -1,3 +1,9 @@
-public class Node {
+public class Node<T> {
+    public T data;
+    public Node<T> next;
 
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
 }
