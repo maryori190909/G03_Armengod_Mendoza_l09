e5474c00343f91c6111522a5b811bafe0c25b83e
maryori190909 <maryori.armengod@ucsm.edu.pe>
2025-06-11 05:21:22 -0500

    v17

--- actividad_01/graph/Edge.java
+++ actividad_01/graph/Edge.java
@@ -1,3 +1,9 @@
-public class Edge {
+public class Edge<E> {
+    private Vertex<E> refDest;
+    private int weight;
 
+    public Edge(Vertex<E> refDest) {
+        this.refDest = refDest;
+        this.weight = -1;
+    }
 }
